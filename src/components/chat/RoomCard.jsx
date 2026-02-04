39bc572d2aca796bbb5ceea4f087904207ee4d33
hardNormalEasyMode <petequdwns12@naver.com>
2026-02-04 15:02:14 +0900

    Update RoomCard.jsx

--- src/components/chat/RoomCard.jsx
+++ src/components/chat/RoomCard.jsx
@@ -43,7 +43,8 @@ const RoomCard = ({ room, onJoin, onEnter }) => {
     return {
       label: "입장 신청",
       disabled: false,
-      className: "bg-white border border-[#101827] text-[#101827] hover:bg-gray-50",
+      className:
+        "bg-white border border-[#101827] text-[#101827] hover:bg-gray-50",
     };
   };
 
